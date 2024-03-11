fcb0da6cc5821e7ac7a34b34372d68757d6e3940
Nikhil Johny Karuthedath <nikhiljohnykaruthedath@Nikhils-MacBook-Pro.local>
2024-03-11 13:37:24 -0500

    Add navigation element and styles

--- src/components/Navigation/Navigation.jsx
+++ src/components/Navigation/Navigation.jsx
@@ -1,9 +1,20 @@
 import React from "react";
+import "./Navigation.css";
+import brandLogo from "/public/images/brand_logo.png";
 
 function Navigation() {
 	return (
 		<>
-			<div>Navigation</div>
+			<nav className="nav container">
+				<img src={brandLogo} alt="" />
+				<ul className="nav__items">
+					<li>Menu</li>
+					<li>Location</li>
+					<li>About</li>
+					<li>Contact</li>
+				</ul>
+				<button className="nav__button">Login</button>
+			</nav>
 		</>
 	);
 }
